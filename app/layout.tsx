4632dbb810cc14474bc05e7a63583a18eb7c868a
K23211 <k23211363.com>
2026-08-08 13:53:12 -0700

    Simplify team cards, remove expandable bios

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import VisitorCounter from "./components/VisitorCounter";
 import { Analytics } from "@vercel/analytics/next";
@@ -53,6 +54,21 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18372099949"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18372099949');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col">
         <main className="flex flex-col flex-1">{children}</main>
         <VisitorCounter />
